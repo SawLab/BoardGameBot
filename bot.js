bcd37ee4c1a10cc5ee4bf9d0392818ab9d9b7e15
Sawyer Labanowski <labanowskisawyer@gmail.com>
2019-06-09 02:28:09 -0500

    Users can now add wins to their total. Leaderboards always print at most, the top 5 scores. Refactored code.

--- bot.js
+++ bot.js
@@ -36,40 +36,97 @@ bot.on('message', function (user, userID, channelID, message, evt) {
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
             case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-			
-			case 'score': 	
-				db.serialize(function() {
-					db.all("SELECT * FROM Users ORDER BY numWins",
-						(error, rows) => {
-							console.log(rows);
-						// receives all the results as an array
-						rows.forEach( row => 
-						bot.sendMessage({
-							to: channelID,
-						    message: `${row.userName}: ${row.numWins}`
-						}));
-					});
-				});
-				break;
-				
+                Ping(channelID);
+				break;			
+			case 'leaderboard': 	
+				TotalWinsLeaderboard(channelID);
+				break;				
 			case 'addme':
-			db.serialize(function() {
-				db.run('INSERT INTO Users(userName, userID) VALUES(?, ?)', [user, userID], function(err) {
-					if(err)
-					{
-						console.error(err.message);
-					}
-					console.log('A row has been inserted with playerName ' + userName + 'and userID ' + id);
-				});
-			});
-            // Just add any case commands if you want to..
+				AddPlayer(user, userID);
+				break;			
+			case 'win':
+				AddPlayerWin(user, userID, channelID);
+				break;
+			default:
+				
          }
      }
 });
+
+function TotalWinsLeaderboard(channelID) {
+	db.serialize(function() {
+		db.all("SELECT userName user, numWins wins FROM Users ORDER BY numWins DESC",
+			(error, rows) => {
+				if (error) {
+					console.error(error.message);
+				}
+				var messageToSend = "";
+				var i = 1;
+				var numToPrint = rows.length < 6 ? rows.length + 1 : 6; //print the top 5 scores; if less than 5 users print the same number as the number users
+				
+				rows.forEach(row => {
+					if (i < numToPrint) {
+						messageToSend = messageToSend + i + ": " + row.user + ": " + row.wins + "\n";
+						i++;
+					}
+				});
+				
+				bot.sendMessage({
+					to: channelID,
+					message: messageToSend
+				});		
+		});
+	});
+}
+
+function AddPlayerWin(user, userID, channelID)
+{
+	db.serialize(function() {
+		let sql = 'SELECT numWins wins FROM Users WHERE userID = ?';
+		db.get(sql, [userID], (err, row) => {
+			if(err) {
+				return console.error(err.message);
+			}
+			
+			let totalWins = row.wins;
+			console.log('Total wins set to ' + totalWins);
+			totalWins = totalWins + 1; //add 1 to the player's total win count
+			console.log('Total wins incremented to ' + totalWins);
+			
+			sql = 'UPDATE Users SET numWins = ? WHERE userID = ?';
+		
+			db.run(sql, [totalWins, userID], function(err) {
+				if (err) {
+					return console.error(err.message);
+				}
+				bot.sendMessage({
+					to: channelID,
+					message: `Congratulations ${user}! Your total wins are now ${totalWins}.`
+				});
+			});
+		});		
+		
+		
+	});
+}
+
+function AddPlayer(user, userID)
+{
+	db.serialize(function() {
+		db.run('INSERT INTO Users(userName, userID) VALUES(?, ?)', [user, userID], function(err) {
+			if(err)
+			{
+				return console.error(err.message);
+			}
+		});
+	});
+}
+
+function Ping(channelID)
+{
+	bot.sendMessage({
+		to: channelID,
+		message: 'Pong!'
+	});
+}
